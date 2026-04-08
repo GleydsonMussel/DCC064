8671c7a1536d933d2c9e608e79f4f8b6f4318c03
GleydsonMussel <gleydson.mussel@estudante.ufjf.br>
2026-04-08 17:29:03 -0300

    Ajustando CamerHandler para ser import'avel dentro do ROS

--- Raspberry/src/camera_pkg/src/send_img_node.py
+++ Raspberry/src/camera_pkg/src/send_img_node.py
@@ -5,11 +5,12 @@ import uuid
 from sensor_msgs.msg import CompressedImage, Image
 from std_msgs.msg import String
 import cv2
-from picamera2 import Picamera2
+from camera_pkg.CameraHandler import CameraHandler
 
 def main():
     # Inicia No
     rospy.init_node('send_img_node')
+    teste = CameraHandler()
     pub = rospy.Publisher('imgs_collected', String, queue_size=10)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
